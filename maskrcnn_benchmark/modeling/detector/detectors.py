74a34665dc2f732dce65b832495daa27407f4197
kongzihan <941141242@qq.com>
2022-10-19 16:29:20 +0800

    给 detectors.py 文件添加注释

--- maskrcnn_benchmark/modeling/detector/detectors.py
+++ maskrcnn_benchmark/modeling/detector/detectors.py
@@ -5,6 +5,13 @@ from .generalized_rcnn import GeneralizedRCNN
 _DETECTION_META_ARCHITECTURES = {"GeneralizedRCNN": GeneralizedRCNN}
 
 
+# cfg为配置信息
+# 根据给定的配置信息实例化一个build_detection_model类对象
+# 该类定义在 ./maskrcnn_benchmark/modeling/detector/generalized_rcnn.py 文件中
+# build_detection_model()这个函数是创建模型的入口函数，也是唯一的模型创建函数
 def build_detection_model(cfg):
+    # 构建一个模型字典
     meta_arch = _DETECTION_META_ARCHITECTURES[cfg.MODEL.META_ARCHITECTURE]
+    # 下面的语句等价于
+    # return GeneralizedRCNN(cfg)
     return meta_arch(cfg)
