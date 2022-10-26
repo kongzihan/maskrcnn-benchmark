fabb35f4fac9d84ff8a7d8bad0870459de1f6881
kongzihan <941141242@qq.com>
2022-10-26 20:01:14 +0800

    给 resnet.py 文件添加注释

--- maskrcnn_benchmark/modeling/backbone/resnet.py
+++ maskrcnn_benchmark/modeling/backbone/resnet.py
@@ -30,24 +30,30 @@ from maskrcnn_benchmark.utils.registry import Registry
 
 
 # ResNet stage specification
+# 文件的开头定义了ResNet的不同stage下的 block 的定义，
+# 使用了 namedtuple 数据结构（命名元组，可以用名字访问元素）来实现
 StageSpec = namedtuple(
     "StageSpec",
     [
-        "index",  # Index of the stage, eg 1, 2, ..,. 5
-        "block_count",  # Number of residual blocks in the stage
-        "return_features",  # True => return the last feature map from this stage
+        "index",  # Index of the stage下标, eg 1, 2, ..,. 5
+        "block_count",  # stage当中的 block 的数量
+        "return_features",  # bool值，若为真，则返回当前 stage 的最后一层的 feature map
     ],
 )
 
 # -----------------------------------------------------------------------------
 # Standard ResNet models
+# 标准 resnet 模块
 # -----------------------------------------------------------------------------
 # ResNet-50 (including all stages)
+# ResNet-50 full stages 的2~5阶段的卷积层数分别为:3,4,6,3
 ResNet50StagesTo5 = tuple(
+    # 元组内部的元素类型为 StageSpec
     StageSpec(index=i, block_count=c, return_features=r)
     for (i, c, r) in ((1, 3, False), (2, 4, False), (3, 6, False), (4, 3, True))
 )
 # ResNet-50 up to stage 4 (excludes stage 5)
+# ResNet-50-C4, 只使用到第四阶段输出的特征图谱
 ResNet50StagesTo4 = tuple(
     StageSpec(index=i, block_count=c, return_features=r)
     for (i, c, r) in ((1, 3, False), (2, 4, False), (3, 6, True))
@@ -63,11 +69,13 @@ ResNet101StagesTo4 = tuple(
     for (i, c, r) in ((1, 3, False), (2, 4, False), (3, 23, True))
 )
 # ResNet-50-FPN (including all stages)
+# 由于FPN需要用到每一个阶段输出的特征图谱, 故 return_features 参数均为 True
 ResNet50FPNStagesTo5 = tuple(
     StageSpec(index=i, block_count=c, return_features=r)
     for (i, c, r) in ((1, 3, True), (2, 4, True), (3, 6, True), (4, 3, True))
 )
 # ResNet-101-FPN (including all stages)
+# ResNet-101-FPN full stages 的卷积层数分别为: 3, 4, 23, 3
 ResNet101FPNStagesTo5 = tuple(
     StageSpec(index=i, block_count=c, return_features=r)
     for (i, c, r) in ((1, 3, True), (2, 4, True), (3, 23, True), (4, 3, True))
@@ -84,38 +92,75 @@ class ResNet(nn.Module):
 
         # If we want to use the cfg in forward(), then we should make a copy
         # of it and store it for later use:
+        # 如果我们希望在 forward 函数中使用 cfg, 那么我们就应该创建一个副本以供其使用
         # self.cfg = cfg.clone()
 
         # Translate string names to implementations
+        # 将配置文件中的字符串转化成具体的实现, 下面三个分别使用了对应的注册模块, 定义在文件的最后
+
+        # 这里是 stem 的实现, 也就是 resnet 的第一阶段 conv1
+        # cfg.MODEL.RESNETS.STEM_FUNC = "StemWithFixedBatchNorm"
         stem_module = _STEM_MODULES[cfg.MODEL.RESNETS.STEM_FUNC]
+
+        # resnet conv2_x~conv5_x 的实现
+        # eg: cfg.MODEL.CONV_BODY="R-50-FPN"
         stage_specs = _STAGE_SPECS[cfg.MODEL.BACKBONE.CONV_BODY]
+
+        # residual transformation function
+        # cfg.MODEL.RESNETS.TRANS_FUNC="BottleneckWithFixedBatchNorm"
         transformation_module = _TRANSFORMATION_MODULES[cfg.MODEL.RESNETS.TRANS_FUNC]
 
+        # 获取上面各个组成部分的实现以后, 就可以利用这些实现来构建模型了
+
         # Construct the stem module
+        # 构建 stem module(也就是 resnet 的stage1, 或者 conv1)
         self.stem = stem_module(cfg)
 
+        # 获取相应的信息来构建 resnet 的其他 stages 的卷积层
+
         # Constuct the specified ResNet stages
+        # 当 num_groups=1 时为 ResNet, >1 时 为 ResNeXt
         num_groups = cfg.MODEL.RESNETS.NUM_GROUPS
         width_per_group = cfg.MODEL.RESNETS.WIDTH_PER_GROUP
+
+        # in_channels 指的是向后面的第二阶段输入时特征图谱的通道数,
+        # 也就是 stem 的输出通道数, 默认为 64
         in_channels = cfg.MODEL.RESNETS.STEM_OUT_CHANNELS
+
+        # 第二阶段输入的特别图谱的通道数
         stage2_bottleneck_channels = num_groups * width_per_group
+
+        # 第二阶段的输出, resnet 系列标准模型可从 resnet 第二阶段的输出通道数判断后续的通道数
+        # 默认为256, 则后续分别为512, 1024, 2048, 若为64, 则后续分别为128, 256, 512
         stage2_out_channels = cfg.MODEL.RESNETS.RES2_OUT_CHANNELS
+
+        # 创建一个空的 stages 列表和对应的特征图谱字典
         self.stages = []
         self.return_features = {}
         for stage_spec in stage_specs:
             name = "layer" + str(stage_spec.index)
+
+            # 计算每个 stage 的输出通道数，每经过一个 stage ，通道数都会加倍
             stage2_relative_factor = 2 ** (stage_spec.index - 1)
+
+            # 计算输入图谱的通道数
             bottleneck_channels = stage2_bottleneck_channels * stage2_relative_factor
+
+            # 计算输出图谱的通道数
             out_channels = stage2_out_channels * stage2_relative_factor
             stage_with_dcn = cfg.MODEL.RESNETS.STAGE_WITH_DCN[stage_spec.index -1]
+
+            # 当获取到所有需要的参数以后, 调用本文件的 `_make_stage` 函数,
+            # 该函数可以根据传入的参数创建对应 stage 的模块(注意是module而不是model)
             module = _make_stage(
                 transformation_module,
-                in_channels,
-                bottleneck_channels,
-                out_channels,
-                stage_spec.block_count,
-                num_groups,
+                in_channels,  # 输入通道数
+                bottleneck_channels,  # 压缩后的通道数
+                out_channels,  # 输出的通道数
+                stage_spec.block_count,  # 当前stage的卷积层数量
+                num_groups,  # ResNet时为1, ResNeXt时>1
                 cfg.MODEL.RESNETS.STRIDE_IN_1X1,
+                # 当处于 stage3~5时, 需要在开始的时候使用 stride=2 来downsize
                 first_stride=int(stage_spec.index > 1) + 1,
                 dcn_config={
                     "stage_with_dcn": stage_with_dcn,
@@ -123,32 +168,48 @@ class ResNet(nn.Module):
                     "deformable_groups": cfg.MODEL.RESNETS.DEFORMABLE_GROUPS,
                 }
             )
+            # 下一个 stage 的输入通道数即为当前 stage 的输出通道数
             in_channels = out_channels
+
+            # 将当前 stage 模块添加到模型中
             self.add_module(name, module)
+
+            # 将 stage 的名称添加到列表中
             self.stages.append(name)
+
+            # 将 stage 的bool值添加到列表中
             self.return_features[name] = stage_spec.return_features
 
         # Optionally freeze (requires_grad=False) parts of the backbone
+        # 根据配置文件的参数选择性地冻结某些层(requires_grad=False)
         self._freeze_backbone(cfg.MODEL.BACKBONE.FREEZE_CONV_BODY_AT)
 
     def _freeze_backbone(self, freeze_at):
+        # 根据给定的参数冻结某些层的参数更新
         if freeze_at < 0:
             return
         for stage_index in range(freeze_at):
             if stage_index == 0:
+                # resnet 的第一阶段, 即为 stem
                 m = self.stem  # stage 0 is the stem
             else:
                 m = getattr(self, "layer" + str(stage_index))
+            # 将 m 中的所有参数置为不更新状态
             for p in m.parameters():
                 p.requires_grad = False
 
+    # 定义 resnet 的前行传播过程
     def forward(self, x):
         outputs = []
+        # 先经过 stem(stage 1)
         x = self.stem(x)
+        # 再依次计算 stage 2-5 的结果
         for stage_name in self.stages:
             x = getattr(self, stage_name)(x)
             if self.return_features[stage_name]:
+                # 将 stage 2-5 的所有计算结果（也就是特征图谱）以列表形式保存
                 outputs.append(x)
+        # 将结果返回，outputs为列表形式，元素为各个stage的特征图谱，刚好作为 FPN 的输入
         return outputs
 
 
@@ -167,19 +228,34 @@ class ResNetHead(nn.Module):
     ):
         super(ResNetHead, self).__init__()
 
+        # 获取不同stage的通道数相对于stage2的倍数
         stage2_relative_factor = 2 ** (stages[0].index - 1)
+
+        # 获取压缩后的 stage2 的 channels
         stage2_bottleneck_channels = num_groups * width_per_group
+
+        # 获取输出的 channels
         out_channels = res2_out_channels * stage2_relative_factor
+
+        # 获取输入的 channels
         in_channels = out_channels // 2
+
+        # 获取压缩后的 channels
         bottleneck_channels = stage2_bottleneck_channels * stage2_relative_factor
 
+        # 根据给定的名称获取相应 block_module
+        # 目前 _TRANSFORMATION_MODULES 只包含 "BottleneckWithFixedBatchNorm" 这一个模块
         block_module = _TRANSFORMATION_MODULES[block_module]
 
+        # 创建一个空的 stages 列表
         self.stages = []
+
+        # 初始化 stride
         stride = stride_init
         for stage in stages:
             name = "layer" + str(stage.index)
             if not stride:
+                # 当处于 stage3~5时, 需要在开始的时候使用 stride=2 来downsize
                 stride = int(stage.index > 1) + 1
             module = _make_stage(
                 block_module,
@@ -198,6 +274,7 @@ class ResNetHead(nn.Module):
             self.stages.append(name)
         self.out_channels = out_channels
 
+    # 定义前向传播过程
     def forward(self, x):
         for stage in self.stages:
             x = getattr(self, stage)(x)
@@ -267,7 +344,7 @@ class Bottleneck(nn.Module):
                         nn.init.kaiming_uniform_(l.weight, a=1)
 
         if dilation > 1:
-            stride = 1 # reset to be 1
+            stride = 1  # reset to be 1
 
         # The original MSRA ResNet models have stride in the first 1x1 conv
         # The subsequent fb.torch.resnet and Caffe2 ResNe[X]t implementations have
@@ -348,20 +425,25 @@ class BaseStem(nn.Module):
     def __init__(self, cfg, norm_func):
         super(BaseStem, self).__init__()
 
+        # resnet-50, out_channels=64
         out_channels = cfg.MODEL.RESNETS.STEM_OUT_CHANNELS
 
+        # 输入的 channels 为 3, 输出为 64
         self.conv1 = Conv2d(
             3, out_channels, kernel_size=7, stride=2, padding=3, bias=False
         )
+
+        # 使用固定参数的 BN 层
         self.bn1 = norm_func(out_channels)
 
-        for l in [self.conv1,]:
+        for l in [self.conv1, ]:
             nn.init.kaiming_uniform_(l.weight, a=1)
 
+    # 定义前向传播过程
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
-        x = F.relu_(x)
+        x = F.relu_(x)  # 原地激活, 因为不含参数, 因此不放在模型定义中, 而放在 forward 中实现
         x = F.max_pool2d(x, kernel_size=3, stride=2, padding=1)
         return x
 
@@ -428,6 +510,7 @@ class StemWithGN(BaseStem):
         super(StemWithGN, self).__init__(cfg, norm_func=group_norm)
 
 
+# 这些模块会通过配置文件中的字符串信息来决定调用哪一个类或者参数
 _TRANSFORMATION_MODULES = Registry({
     "BottleneckWithFixedBatchNorm": BottleneckWithFixedBatchNorm,
     "BottleneckWithGN": BottleneckWithGN,
@@ -438,6 +521,7 @@ _STEM_MODULES = Registry({
     "StemWithGN": StemWithGN,
 })
 
+# 定义各个 ResNet 模型的 stages 的卷积层数量
 _STAGE_SPECS = Registry({
     "R-50-C4": ResNet50StagesTo4,
     "R-50-C5": ResNet50StagesTo5,
