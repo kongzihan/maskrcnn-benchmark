4d7b9979b938784ced7d595658ab4b2408787387
kongzihan <941141242@qq.com>
2022-10-26 20:00:58 +0800

    给 backbone.py 文件添加注释

--- maskrcnn_benchmark/modeling/backbone/backbone.py
+++ maskrcnn_benchmark/modeling/backbone/backbone.py
@@ -1,32 +1,45 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# 导入有序字典
 from collections import OrderedDict
 
 from torch import nn
 
+# 注册器，用于管理 module 的注册，使得可以像使用字典一样使用 module
 from maskrcnn_benchmark.modeling import registry
 from maskrcnn_benchmark.modeling.make_layers import conv_with_kaiming_uniform
+
+# fpn和resnet都是同文件夹下的文件
 from . import fpn as fpn_module
 from . import resnet
 
 
+# 创建 resnet 骨架网络，根据配置信息会被后面的 build_backbone () 函数调用
 @registry.BACKBONES.register("R-50-C4")
 @registry.BACKBONES.register("R-50-C5")
 @registry.BACKBONES.register("R-101-C4")
 @registry.BACKBONES.register("R-101-C5")
 def build_resnet_backbone(cfg):
+    # resnet.py 文件中的 class ResNet(cfg)
     body = resnet.ResNet(cfg)
+    # 利用 nn.Sequential 定义模型
     model = nn.Sequential(OrderedDict([("body", body)]))
     model.out_channels = cfg.MODEL.RESNETS.BACKBONE_OUT_CHANNELS
     return model
 
 
+# 创建 FPN 网络，根据配置信息会被下面 build_backbone() 函数调用
 @registry.BACKBONES.register("R-50-FPN")
 @registry.BACKBONES.register("R-101-FPN")
 @registry.BACKBONES.register("R-152-FPN")
 def build_resnet_fpn_backbone(cfg):
+    # 先创建 resnet 网络
     body = resnet.ResNet(cfg)
+
+    # 获取 FPN 所需的channels 参数
     in_channels_stage2 = cfg.MODEL.RESNETS.RES2_OUT_CHANNELS
     out_channels = cfg.MODEL.RESNETS.BACKBONE_OUT_CHANNELS
+
+    # 利用 fpn.py 文件夹中的 class FPN 创建 FPN 网络
     fpn = fpn_module.FPN(
         in_channels_list=[
             in_channels_stage2,
